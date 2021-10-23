b925ab165c90eee1df39272de948ac8a617c4e6f
AGsPortfolio <64134099+AGsPortfolio@users.noreply.github.com>
2021-10-23 15:47:17 +0200

    Keep on coding

--- form-validation.js
+++ form-validation.js
@@ -1,15 +1,15 @@
 function firstNameValidation (first) {
     var firstNameFormat = /^[ a-zA-ZàèìòùÀÈÌÒÙáéíóúýÁÉÍÓÚÝâêîôûÂÊÎÔÛãñõÃÑÕäëïöüÿÄËÏÖÜŸçÇßØøÅåÆæœ'`'\-]+$/;
     if(first.value.match(firstNameFormat)) {
-        document.getElementById('firstNameError').innerHTML="First name cannnot be empty";
+        document.getElementById('firstNameMessage').style="visibility:hidden";
         return true;
     }
     if(first.value == "") {
-        document.getElementById('firstNameError');
+        document.getElementById('firstNameMessage').style="visibility:visible";
         return false;
     }
     else {
-        document.getElementById('first-name').value="Please enter a valid first name";
+        document.getElementById('firstNameMessage').style="visibility:visible"
         return false;
     }
 }
@@ -17,15 +17,15 @@ function firstNameValidation (first) {
 function lastNameValidation (last) {
     var lastNameFormat = /^[ a-zA-ZàèìòùÀÈÌÒÙáéíóúýÁÉÍÓÚÝâêîôûÂÊÎÔÛãñõÃÑÕäëïöüÿÄËÏÖÜŸçÇßØøÅåÆæœ'`'\-]+$/;
     if(last.value.match(lastNameFormat)) {
-        document.getElementById('last-name').value=last.value;
+        document.getElementById('lastNameMessage').style="visibility:hidden";
         return true;
     }
     if(last.value == "") {
-        document.getElementById('last-name').value="Last name field cannot be empty";
+        document.getElementById('lastNameMessage').style="visibility:visible";
         return false;
     }
     else {
-        document.getElementById('last-name').value="please enter a valid last name";
+        document.getElementById('lastNameMessage').style="visibility:visible";
         return false;
     }
 }
@@ -33,15 +33,15 @@ function lastNameValidation (last) {
 function emailValidation (email) {
     var mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; 
     if (email.value.match(mailFormat)) {
-        document.getElementById('email-address').value=email.value;
+        document.getElementById('emailMessage').style="visibility:hidden";
         return true;  
     }
     if(email.value == "") {
-        document.getElementById('email-address').value="Email field cannot be empty";
+        document.getElementById('emailMessage').style="visibility:visible";
         return false;
     }
     else {
-        document.getElementById('email-address').value="Please enter a valid email address";
+        document.getElementById('emailMessage').style="visibility:visible";
         return false;
     }
 }
@@ -49,15 +49,15 @@ function emailValidation (email) {
 function passwordValidation (password) {
     var passwordRequirment = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,14}$/;
     if (password.value.match(passwordRequirment)) {
-        document.getElementById('password').value=password.value;
+        document.getElementById('passwordMessage').style="visibility:hidden";
         return true;
     }
     if (password.value == "") {
-        document.getElementById('password').value="Password field cannot be empty";
+        document.getElementById('passwordMessage').style="visibility:visible";
         return false;
     }
     else {
-        document.getElementById('password').value="Password must contain at least 8 characters, a number, and 1 special character (!@#$%^&*)";
+        document.getElementById('passwordMessage').style="visibility:visible";
         return false;
     }    
 }
